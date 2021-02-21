eb9854cf0571bff1f9159b403bd8c910f9198e5c
Ali Kazan <alikazan15@gmail.com>
2021-02-21 06:27:03 +0200

    Global Scope

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -2,144 +2,140 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Offer;
-use App\Models\Video;
 use App\Events\VideoViewer;
 use App\Http\Requests\OfferRequest;
-use Illuminate\Http\Request;
+use App\Models\Offer;
+use App\Models\Video;
+use App\Scopes\OfferScope;
 use App\Traits\OfferTrait;
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-
 use LaravelLocalization;
 
 class CrudController extends Controller
 {
+
     use OfferTrait;
+
+    //photo
+    //update
+    //delete
+    //upload image
+    //improve code
 
     public function __construct()
     {
-       
+
     }
 
     public function getOffers()
     {
-        return Offer::select('id','name') ->get();
-
+        return Offer::select('id', 'name')->get();
     }
 
-    /*public function store()
-    {
-        Offer::create([
 
-            'name' => 'Offer3',
-            'price' => '5000',
-            'details' => 'Offer details',
-        ]);
-    }
-*/
+    /* public function store()
+     {
+
+         Offer::create([
+             'name' => 'Offer3',
+             'price' => '5000',
+             'details' => 'offer details',
+         ]);
+     }*/
+
+
     public function create()
     {
         return view('offers.create');
-
     }
 
-   public function store(OfferRequest $request)
-   {
-        
-    
-    
-    
-    //return $request;
-    
-    //validate data before insert to database
 
-      //  $rules= $this -> getRules();
-       // $messages= $this -> getMessages();
-       // $validator= Validator::make($request ->all(),$rules, $messages);
-
-      //  if($validator ->fails())
-      //  {
-      //      return redirect()->back() ->withErrors($validator)->withInputs($request ->all());
-      //  }
-        
-      
-      $file_name = $this->saveImage($request->photo,'images/offers');
-        
+    public function store(OfferRequest $request)
+    {
+        //validate data before insert to database
+        //$rules = $this->getRules();
+        //$messages = $this->getMessages();
+        // $validator = Validator::make($request->all() ,$rules, $messages);
+        // if ($validator->fails()) {
+        //    return redirect()->back()->withErrors($validator)->withInputs($request->all());
+        // }
 
 
+        $file_name = $this->saveImage($request->photo, 'images/offers');
 
-
-    //insert 
-
+        //insert
         Offer::create([
-
             'photo' => $file_name,
             'name_ar' => $request->name_ar,
             'name_en' =>   $request->name_en,
             'price' =>  $request->price,
             'details_ar' => $request->details_ar,
             'details_en' => $request->details_en,
-
         ]);
 
-       // return 'saved successfaly';
-
-       return redirect()->back() ->with(['success' =>'تم اضافة العرض بنجاح']);
-       
-   }
-
- /*  protected function getMessages(){
-
-        return $messages=[
-
-            'name.required' =>__( 'messages.offer name required'),
-            'name.unique' => __('messages.offer name must be unique'),
-            'price.numeric' => __('messages.price must be numeric'),
-            'price.required' => __('messages.price is required'),
-            'details.required' => __('messages.details is required'),
-        ];
-   }
+        return redirect()->back()->with(['success' => 'تم اضافه العرض بنجاح ']);
+    }
 
 
-   protected function getRules(){
+    /*
+        protected function getMessages()
+        {
 
-    return $rules =[
+            return $messages = [
+                'name.required' => __('messages.offer name required'),
+                'name.unique' => 'اسم العرض موجود ',
+                'price.numeric' => 'سعر العرض يجب ان يكون ارقام',
+                'price.required' => 'السعر مطلوب',
+                'details.required' => 'ألتفاصيل مطلوبة ',
+            ];
+        }
 
-        'name' => 'required|max:100|unique:offers,name',
-        'price' =>'required|numeric',
-        'details' => 'required' ,
+        protected function getRules()
+        {
 
-    ];
+            return $rules = [
+                'name' => 'required|max:100|unique:offers,name',
+                'price' => 'required|numeric',
+                'details' => 'required',
+            ];
+        }*/
 
-
-   }
-
-   */
-
-  public function getAllOffers(){
-
-   // $offers = Offer::select('id','name_ar','name_en','price','details_ar','details_en')->get(); /*return collection of all result*/
-
-     $offers = Offer::select('id',
+    public function getAllOffers()
+    {
+       /* $offers = Offer::select('id',
             'price',
+            'photo',
             'name_' . LaravelLocalization::getCurrentLocale() . ' as name',
             'details_' . LaravelLocalization::getCurrentLocale() . ' as details'
-        )->get(); // return collection of all result
+        )->get(); // return collection of all result*/
 
 
-    return view('offers.all', compact('offers'));
-    
-  }
+       ##################### paginate result ####################
+         $offers = Offer::select('id',
+            'price',
+            'photo',
+            'name_' . LaravelLocalization::getCurrentLocale() . ' as name',
+            'details_' . LaravelLocalization::getCurrentLocale() . ' as details'
+        )->paginate(PAGINATION_COUNT);
 
-  public function editOffer($offer_id)
+
+
+        //return view('offers.all', compact('offers'));
+
+
+        return view('offers.paginations',compact('offers'));
+    }
+
+
+    public function editOffer($offer_id)
     {
         // Offer::findOrFail($offer_id);
         $offer = Offer::find($offer_id);  // search in given table id only
         if (!$offer)
-            return redirect()->back(); // if id does not existe back 
+            return redirect()->back();
 
-        $offer = Offer::select('id', 'name_ar', 'name_en', 'details_ar', 'details_en', 'price','photo')->find($offer_id);
+        $offer = Offer::select('id', 'name_ar', 'name_en', 'details_ar', 'details_en', 'price')->find($offer_id);
 
         return view('offers.edit', compact('offer'));
 
@@ -185,9 +181,6 @@ class CrudController extends Controller
           ]);*/
 
     }
-        
-
-
 
 
     public function getVideo()
@@ -197,6 +190,21 @@ class CrudController extends Controller
         return view('video')->with('video', $video);
     }
 
-   
 
+    public function getAllInactiveOffers(){
+
+          // where  whereNull whereNotNull whereIn
+          //Offer::whereNotNull('details_ar') -> get();
+
+         //return  $inactiveOffers = Offer::inactive()->get();  //all inactive offers
+
+                                    // global scope
+        // return  $inactiveOffers = Offer::get();  //all inactive offers
+
+                // how to  remove global scope
+
+            return $offer  = Offer::withoutGlobalScope(OfferScope::class)->get();
+
+
+    }
 }
--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\OfferScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Offer extends Model
@@ -9,5 +10,34 @@ class Offer extends Model
     protected $table = "offers";
     protected $fillable = ['name_ar', 'photo', 'name_en', 'price', 'details_ar', 'details_en', 'created_at', 'updated_at', 'status'];
     protected $hidden = ['created_at', 'updated_at'];
-    
+
+    // public $timestamps = true;
+
+    //register global scope
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new OfferScope);
+    }
+
+
+    ######################### local scopes ####################
+    public function scopeInactive($query)
+    {
+        return $query->where('status', 0);
+    }
+
+    public function scopeInvalid($query)
+    {
+        return $query->where('status', 0)->whereNull('details_ar');
+    }
+
+    #########################################################
+
+    //mutators
+
+    public function setNameEnAttribute($value)
+    {
+        $this->attributes['name_en'] = strtoupper($value);
+    }
 }
